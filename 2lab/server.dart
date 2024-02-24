effd290b838b2701215a79911920854013162065
nastya <akkroys@gmail.com>
2024-02-24 12:13:51 +0300

    corrected

--- 2lab/server.dart
+++ 2lab/server.dart
@@ -3,49 +3,54 @@ import 'dart:convert';
 import 'package:udp/udp.dart';
 
 void main() async {
-  var multicastEndpoint =
-      Endpoint.multicast(InternetAddress("239.1.2.3"), port: Port(54321));
+  var unicastEndpoint =
+      Endpoint.unicast(InternetAddress("127.0.0.1"), port: Port(54321));
 
-  var receiver = await UDP.bind(multicastEndpoint);
+  var receiver = await UDP.bind(unicastEndpoint);
 
   var sender = await UDP.bind(Endpoint.any());
 
-  var responseReceiver = await UDP.bind(Endpoint.any());
+  bool exitRequested = false;
 
-  print('Enter a string to send to the server:');
-  var input = stdin.readLineSync();
-  if (input != null) {
-    print('Sending: $input');
-    await sender.send(utf8.encode(input), multicastEndpoint);
-    await Future.delayed(Duration(seconds: 1));
+  while (!exitRequested) {
+    print('Enter a string to send to the server, or type "exit" to quit:');
+    var input = stdin.readLineSync();
+    if (input != null) {
+      if (input.toLowerCase() == 'exit') {
+        exitRequested = true;
+        continue;
+      }
+      print('Sending: $input');
+      await sender.send(utf8.encode(input), unicastEndpoint);
+      await Future.delayed(Duration(seconds: 1));
+    }
+
+    receiver.asStream().listen((datagram) async {
+      if (datagram != null) {
+        var str = String.fromCharCodes(datagram.data);
+        var modifiedString = swapCharacters(str);
+        print('Received: $str');
+        await Future.delayed(Duration(seconds: 1));
+        print('Modified: $modifiedString');
+        var clientEndpoint =
+            Endpoint.unicast(datagram.address, port: Port(datagram.port));
+        receiver.send(utf8.encode(modifiedString), clientEndpoint);
+      }
+    });
+
+    sender.asStream().listen((event) async {
+      if (event != null) {
+        var modifiedStr = utf8.decode(event.data);
+        print('Response from server: $modifiedStr');
+      }
+    });
+
+    await Future.delayed(Duration(seconds: 3));
   }
 
-
-  receiver.asStream().listen((datagram) async {
-    if (datagram != null) {
-      var str = String.fromCharCodes(datagram.data);
-      var modifiedString = swapCharacters(str);
-      print('Received: $str');
-      await Future.delayed(Duration(seconds: 1));
-      print('Modified: $modifiedString');
-      var clientEndpoint =
-          Endpoint.unicast(datagram.address, port: Port(datagram.port));
-      receiver.send(utf8.encode(modifiedString), clientEndpoint);
-    }
-  });
-
-  sender.asStream().listen((event) async {
-    if (event != null) {
-      var modifiedStr = utf8.decode(event.data);
-      print('Response from server: $modifiedStr');
-    }
-  });
-
-  await Future.delayed(Duration(seconds: 3));
-
+  print('Exiting...');
   sender.close();
   receiver.close();
-  responseReceiver.close();
 }
 
 String swapCharacters(String str) {
